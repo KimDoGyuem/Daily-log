9613c1abef800ebef75ece434cef9756829d77a6
KimDoGyuem <dkdiskal357@gmail.com>
2024-12-06 16:16:25 +0900

    Delete 2024.12.04 Work/ReactProject01/ReactProject01 directory

--- 2024.12.04 Work/ReactProject01/ReactProject01/src/main/java/com/project/mh/board/dto/AccountDto.java
+++
@@ -1,9 +0,0 @@
-package com.project.mh.board.dto;
-
-import lombok.Data;
-
-@Data
-public class AccountDto {
-	private String id;
-	private String pw;
-}
--- 2024.12.04 Work/ReactProject01/ReactProject01/src/main/java/com/project/mh/dto/MHOcidDto.java
+++
@@ -1,6 +0,0 @@
-package com.project.mh.dto;
-
-public class MHOcidDto {
-
-	public String ocid;
-}
